5d8673d3a95a271d05f23e07d2b741b8e5b68787
danSambajon <sambajondanaron@gmail.com>
2024-05-03 15:15:25 +0800

    Add project files.

---
+++ Models/RowsCount.cs
@@ -0,0 +1,7 @@
+﻿namespace CSVGenerator.Models;
+    public class RowsCount
+    {
+        public int fuels { get; set; }
+        public int Lubes { get; set; }
+        public int safedrops { get; set; }
+    }
